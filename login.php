1f25ebd1e224adfd3c76a8274c3e695adb4bc91c
SakethKenchem <saketh.kenchem@strathmore.edu>
2025-01-06 12:14:27 +0300

    comment out debug output and update styling for OTP verification page

--- login.php
+++ login.php
@@ -163,7 +163,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </form>
             <?php if ($message = $login->getMessage()): ?>
                 <div class="alert alert-danger mt-3">
-                    <?= htmlspecialchars($message) ?>
+                    <?= ($message) ?>
                 </div>
             <?php endif; ?>
         </div>
