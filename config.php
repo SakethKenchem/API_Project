88091ca4948ce35dc73dc2a35dc6952a04f1d9af
SakethKenchem <saketh.kenchem@strathmore.edu>
2025-01-08 12:12:20 +0300

    comment change

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-// Base URL for your project
+// Base URL for project
 define('BASE_URL', '/API_Project/');
 
-// Database connection details (adjust according to your setup)
+// Database connection details
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'project_api');
 define('DB_USER', 'root');
 define('DB_PASS', 'S00per-d00per');
 
-// Other settings can go here
